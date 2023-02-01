70544c317d6ff52b731b3b223807da13689beb6c
CoderRo <80358945+RoWaysek@users.noreply.github.com>
2023-02-02 03:11:56 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class exercise1
+{
+    public static void Main()
+
+    {
+        int[] arr = new int[10];
+        Console.WriteLine("Read and print elements of an array");
+        Console.WriteLine("-----------------------------------");
+
+        Console.WriteLine("Write the elements: ");
+        for(int i = 0; i < 10; i++)
+        {
+            Console.Write("The {0} element is - ", i);
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.Write("The elements of the array are: ");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write(arr[i]);
+        }
+    }
+    
+
+}
+
